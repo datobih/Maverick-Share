6f4ac3a19f5e16c066ca7d8477b37589db80c398
Ayodele David <dayodele89@outlook.com>
2022-01-03 22:53:54 +0100

    Added Queue File Recycler Adapter

--- app/src/main/java/com/example/maverickfilesender/adapters/QueueFileRecyclerAdapter.kt
+++ app/src/main/java/com/example/maverickfilesender/adapters/QueueFileRecyclerAdapter.kt
@@ -1,20 +1,44 @@
 package com.example.maverickfilesender.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.maverickfilesender.R
+import com.example.maverickfilesender.model.FileMetaData
+import kotlinx.android.synthetic.main.item_queue_files.view.*
 import java.io.File
 
-class QueueFileRecyclerAdapter(val context: Context,val fileList:ArrayList<File>) :RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+class QueueFileRecyclerAdapter(val context: Context,val fileMetaDataList:ArrayList<FileMetaData>) :RecyclerView.Adapter<RecyclerView.ViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+return MyQueueViewHolder(LayoutInflater.from(context).inflate(R.layout.item_queue_files,parent,false))
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        if(holder is MyQueueViewHolder){
+        if(fileMetaDataList[position].bitmap!=null){
+            holder.itemView.imv_queue_file.setImageBitmap(fileMetaDataList[position].bitmap)
+        }
+            else{
+
+        }
+
+            holder.itemView.tv_item_queueFile_name.text=fileMetaDataList[position].name
+
+            holder.itemView.tv_item_queueFile_totalSize.text=fileMetaDataList[position].size.toString()
+
+
+        }
+
+
     }
 
     override fun getItemCount(): Int {
-return fileList.size
+return fileMetaDataList.size
     }
+
+
+    class MyQueueViewHolder(view: View):RecyclerView.ViewHolder(view)
+
 }
